8dd1ef1299433491e2a03f95115c3f9c348a1cc3
adeelnasimsyed <47543697+adeelnasimsyed@users.noreply.github.com>
2020-02-15 16:49:35 -0500

    Create frequentWords.py

---
+++ frequentWords.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+#returns the first k most frequent words in a string(same frequency ordered alphabetically)
+def frequentWords(words, k):
+
+	d = defaultdict(int)
+
+	for word in words.split(" "):
+		word = word.lower().strip(",!_+=-@")
+
+		d[word] += 1
+
+
+	print(d)
+	output = [v[0] for v in sorted(d.items(), key = lambda kv: (-kv[1], kv[0]), reverse=False)]
+
+	print(output[:k])
+
+
+a = "apple apple apple azdy azdy azdy Gabba gabba hey, gabba gabba hey!"
+
+frequentWords(a,2)
+
