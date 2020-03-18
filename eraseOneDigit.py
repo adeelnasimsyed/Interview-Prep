a141e30b127ce184c399dd4c98ed31d438c9c90c
Adeel <adeel.nasimsyed@gmail.com>
2020-03-18 19:28:13 -0400

    added eraseOneDigit.py

---
+++ eraseOneDigit.py
@@ -0,0 +1,14 @@
+def eraseOneDigit(firstNum, secondNum, thirdNum):
+
+	target = str(int(thirdNum) - int(secondNum))
+
+	print(target)
+
+
+
+
+firstNum = "1000"
+secondNum = "20"
+thirdNum = "90"
+
+eraseOneDigit(firstNum, secondNum, thirdNum)
